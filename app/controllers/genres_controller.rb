10dd5c19ef37a2e41fa097dd39fa85a85dc12ca3
Ryo-he <suzuky12020915@gmail.com>
2022-05-17 14:34:13 +0000

    [0517 commit 7]

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,4 @@
 class GenresController < ApplicationController
-  before_action :user_admin, only: [:index,:create]
 
   def index
     @genre = Genre.new
@@ -41,10 +40,5 @@ class GenresController < ApplicationController
     params.require(:genre).permit(:name)
   end
   
-  def user_admin
-   unless current_user.admin?
-     redirect_to root_path 
-   end
-  end
 
 end

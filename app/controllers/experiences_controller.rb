cbf22a7d66008cdea89df687f4e2af78e33a349a
Ryo-he <suzuky12020915@gmail.com>
2022-05-21 13:47:32 +0000

    [index concern]

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,67 +4,8 @@ class ExperiencesController < ApplicationController
     @genres = Genre.all
   end
 
-  def index
-    @experience = Experience.new
-    @keyword = params[:keyword]
-   if params[:genres]
-    if params[:genres].class == Array
-      @genres = params[:genres].join(",")
-    else
-      @genres = params[:genres]
-    end
-   end
-   if params[:sort].nil? && params[:genres].nil? && params[:keyword].nil?
-      @experiences = Experience.page_new(params[:page])
-   elsif params[:sort] == 'new'
-    if params[:genres] && params[:genres] != ""
-      if params[:genres].class == Array
-        @experiences = Experience.where(genre_id: params[:genres]).page_new(params[:page])
-      else
-        @experiences = Experience.where(genre_id: params[:genres].split(",")).page_new(params[:page])
-      end
-    elsif params[:keyword]
-      @experiences = Experience.search(params[:keyword]).page_new(params[:page])
-    else
-      @experiences = Experience.page_new(params[:page])
-    end
-   elsif params[:sort] == 'old'
-     if params[:genres] && params[:genres] != ""
-      if params[:genres].class == Array
-        @experiences = Experience.where(genre_id: params[:genres]).page_old(params[:page])
-      else
-        @experiences = Experience.where(genre_id: params[:genres].split(",")).page_old(params[:page])
-      end
-     elsif params[:keyword]
-      @experiences = Experience.search(params[:keyword]).page_old(params[:page])
-     else
-      @experiences = Experience.page_old(params[:page])
-     end
-   elsif params[:sort] == 'favorite'
-     if params[:genres] && params[:genres] != ""
-      if params[:genres].class == Array
-        @experiences = Experience.where(genre_id: params[:genres]).sort_like(params[:page])
-      else
-        @experiences = Experience.where(genre_id: params[:genres].split(",")).sort_like(params[:page])
-      end
-     elsif params[:keyword]
-       @experiences = Experience.search(params[:keyword]).sort_like(params[:page])
-     else
-      @experiences = Experience.sort_like(params[:page])
-     end
-   elsif params[:sort] == "" && params[:genres] == ""
-      @experiences = Experience.page_new(params[:page])
-   elsif params[:genres]
-      if params[:genres] == [""]
-       @experiences = Experience.page_new(params[:page])
-      else
-         @experiences = Experience.where(genre_id: params[:genres]).page_new(params[:page])
-      end
-   elsif params[:keyword]
-      @experiences = Experience.search(params[:keyword]).page_new(params[:page])
-   end
-  end
-
+  include Sort
+ 
   def create
     @experience = Experience.new(experience_params)
     @experience.user_id = current_user.id
